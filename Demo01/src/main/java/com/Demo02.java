77c58648365a5dd8c55c68731614cf667a0bbbd9
liling <1139608130@qq.com>
2023-08-09 23:14:52 +0800

    添加javaTest2

---
+++ Demo01/src/main/java/com/Demo02.java
@@ -0,0 +1,16 @@
+package com;
+
+/**
+ * ClassName: Demo02
+ * Package: com
+ * Description:
+ *
+ * @author£∫¿Ë¡·
+ * @Creat: 2023/8/9-- 23:13
+ * @Version: v1.0
+ */
+public class Demo02 {
+    public static void main(String[] args) {
+        System.out.println("java Test2");
+    }
+}
