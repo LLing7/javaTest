952bbd383388bf510a512dfa8eb96c41e5ede06c
liling <1139608130@qq.com>
2023-08-09 23:31:07 +0800

    修改demo01

---
+++ Demo01/src/main/java/com/Demo01.java
@@ -0,0 +1,26 @@
+package com;
+
+/**
+ * ClassName: Demo01
+ * Package: com
+ * Description:
+ *
+ * @author：黎玲
+ * @Creat: 2023/8/9-- 22:15
+ * @Version: v1.0
+ */
+public class Demo01 {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+    public  int div(int a,int b)
+    {
+        return a/b;
+    }
+    //添加了一行没有用的注释
+
+}
