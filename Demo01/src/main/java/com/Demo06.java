f940afc3299b0ad33d29c47a02c59b99a98dbd2a
liling <1139608130@qq.com>
2023-08-13 21:37:27 +0800

    添加了Demo05和Demo06

---
+++ Demo01/src/main/java/com/Demo06.java
@@ -0,0 +1,16 @@
+package com;
+
+/**
+ * ClassName: Demo06
+ * Package: com
+ * Description:
+ *
+ * @author£∫¿Ë¡·
+ * @Creat: 2023/8/13-- 21:31
+ * @Version: v1.0
+ */
+public class Demo06 {
+    public static void main(String[] args) {
+        System.out.println("Demo06");
+    }
+}
