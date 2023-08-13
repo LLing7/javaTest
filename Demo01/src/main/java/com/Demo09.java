203d5958acf4a572c164fbc06fa49053688eff81
liling <1139608130@qq.com>
2023-08-13 21:53:18 +0800

    Demo09

---
+++ Demo01/src/main/java/com/Demo09.java
@@ -0,0 +1,16 @@
+package com;
+
+/**
+ * ClassName: Demo09
+ * Package: com
+ * Description:
+ *
+ * @author£∫¿Ë¡·
+ * @Creat: 2023/8/13-- 21:52
+ * @Version: v1.0
+ */
+public class Demo09 {
+    public static void main(String[] args) {
+        System.out.println("Demo09");
+    }
+}
